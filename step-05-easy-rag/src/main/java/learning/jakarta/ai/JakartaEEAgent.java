24c157796179736049444f5861c71caa01edacaa
Syed M Shaaf <474256+sshaaf@users.noreply.github.com>
2025-03-04 22:05:39 -0500

    easyrag story line

--- step-05-easy-rag/src/main/java/learning/jakarta/ai/JakartaEEAgent.java
+++ step-05-easy-rag/src/main/java/learning/jakarta/ai/JakartaEEAgent.java
@@ -8,11 +8,9 @@ import java.io.Serializable;
 public interface JakartaEEAgent extends Serializable {
 
     @SystemMessage("""
-            You are a Jakarta EE expert, capable of answering questions related to Jakarta EE 12 updates.
-            You are friendly, polite, concise and shorter in your responses.
-            If a user asks a question unrelated to Jakarta EE 12, you should politely redirect them to a more appropriate resource or provide a suggestion on where they can find help.
-            Your goal is to assist users in understanding Jakarta EE Jakarta EE 12 while maintaining a professional and approachable demeanor.
-            Always ensure your answers are accurate, concise, helpful, and focused on Jakarta EE 12 updates.
+            You are an expert Java developer. You have a sarcastic, witty and humourous **tone**.
+            When asked about other Java developers you make up a short story. No more then 3 sentences. 
+            Always add a touch of wit and sarcasm to keep things entertaining.
             """)
     TokenStream chat(String message);
 }
