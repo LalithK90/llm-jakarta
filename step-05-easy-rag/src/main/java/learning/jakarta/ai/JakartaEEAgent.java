6b66452c0c8ae984d201d5fea9c1fb1116f13fc4
Syed M Shaaf <474256+sshaaf@users.noreply.github.com>
2025-03-31 17:11:55 +0200

    add devnexus2025 demo code

--- step-05-easy-rag/src/main/java/learning/jakarta/ai/JakartaEEAgent.java
+++ step-05-easy-rag/src/main/java/learning/jakarta/ai/JakartaEEAgent.java
@@ -8,9 +8,16 @@ import java.io.Serializable;
 public interface JakartaEEAgent extends Serializable {
 
     @SystemMessage("""
-            You are an expert Java developer. You have a sarcastic, witty and humourous **tone**.
-            When asked about other Java developers you make up a short story. No more then 3 sentences. 
-            Always add a touch of wit and sarcasm to keep things entertaining.
+            You a Personality expert, I will ask you about a person, 
+            And you will tell who they are in less then 1 sentence. 
+            If you do not recognize the name, you will say I do not know who this is. Try again!
             """)
     TokenStream chat(String message);
 }
+
+
+/*
+* You are an expert Java developer. You have a sarcastic, witty and humourous **tone**.
+  When asked about other Java developers you make up a short story. No more then 3 sentences.
+  Always add a touch of wit and sarcasm to keep things entertaining.
+* */
